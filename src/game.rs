e9ebadf7325c3f7d3d45c5bb7e606c1c3c4064c4
Nicole Venner <nvenner@protonmail.ch>
2025-10-03 11:25:02 -0600

    a fuckton of logic changes

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,9 @@
+use std::{collections::BTreeMap, hash::Hash};
+
+use bevy::utils::hashbrown::HashMap;
+use indexmap::IndexMap;
+use thiserror::Error;
+
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Player {
     Black,
@@ -40,14 +46,6 @@ impl<const SIZE: usize> Board<SIZE> {
     }
 }
 
-pub struct GameNode<const SIZE: usize> {
-    pub board: Board<SIZE>,
-    pub captures: (u32, u32), // (black, white)
-    pub ko_point: Option<(usize, usize)>,
-    pub move_info: Option<Move>, // None for the root node
-    pub children: Vec<GameNode<SIZE>>,
-}
-
 impl<const SIZE: usize> GameNode<SIZE> {
     pub fn legal_moves(&self) -> Vec<(usize, usize)> {
         let mut moves = Vec::new();
@@ -76,7 +74,7 @@ impl<const SIZE: usize> GameNode<SIZE> {
         // 2. Checking the liberties of the new stone's group.
         // 3. If the group has no liberties, checking if the move captures any opponent stones.
         // A move is suicide if the group has 0 liberties AND it captures no opponent stones.
-        
+
         // For now, we'll just prevent placing a stone on another stone.
         self.board.get_point(coords.0, coords.1) == Some(Point::Empty)
     }
@@ -91,9 +89,105 @@ impl<const SIZE: usize> GameNode<SIZE> {
 }
 
 #[derive(Clone, Copy, Debug)]
-pub struct Move {
+pub struct BadukMove {
     pub player: Player,
     pub coordinates: (usize, usize),
 }
 
-pub type GameTree<const SIZE: usize> = GameNode<SIZE>;
+#[derive(Error, Debug)]
+enum MoveError {
+    #[error("Illegal Move")]
+    IllegalMove,
+    #[error("Unexpected Missing Move in Game Tree")]
+    MissingMove,
+}
+
+trait StatelessGame: Sized + Clone {
+    type Move: Hash + Eq + Copy;
+    fn list_all_legal_moves(&self) -> Vec<Self::Move>;
+    fn is_legal(&self, game_move: &Self::Move) -> bool;
+    fn generate_next_board(&self, game_move: &Self::Move) -> Result<Self, MoveError>;
+}
+
+pub struct BadukClassical<const SIZE: usize> {
+    pub turn: Player,
+    pub board: Board<SIZE>,
+    pub captures: (u32, u32), // (black, white)
+    pub ko_point: Option<(usize, usize)>,
+}
+
+pub enum SupportedGames {
+    BadukClassic(BadukClassical<19>),
+    BadukBeginner(BadukClassical<13>),
+    BadukNewbie(BadukClassical<9>),
+}
+
+#[derive(Clone)]
+pub struct GameNode<G: StatelessGame> {
+    pub game: G,
+    // Maybe its a good idea to swap this out for a hashmap, but I think since most of these are
+    // going to only contain 5-10 elements it might be faster as a btree, and it also ensures that
+    // they can be displayed and saved in a consistent order.
+    //
+    // Also the game G gets saved twice, once as the key in the btree and another as the
+    // GameNode.game.
+    pub children: IndexMap<G::Move, GameNode<G>>,
+}
+impl<G: StatelessGame> GameNode<G> {
+    fn new(game: G) -> Self {
+        GameNode {
+            game,
+            children: IndexMap::new(),
+        }
+    }
+    fn traverse_downward(&mut self, mv: &G::Move) -> Result<&mut Self, MoveError> {
+        self.children.get_mut(mv).ok_or(MoveError::MissingMove)
+    }
+
+    fn make_move(&mut self, mv: G::Move) -> Result<&mut Self, MoveError> {
+        if !self.game.is_legal(&mv) {
+            return Err(MoveError::IllegalMove);
+        }
+        let generate_board = || {
+            let current_board = &self.game;
+            let next_board = current_board.generate_next_board(&mv).unwrap();
+            GameNode::new(next_board)
+        };
+        let next_node_ref = self.children.entry(mv).or_insert_with(generate_board);
+        Ok(next_node_ref)
+    }
+}
+
+pub struct GamePointer<G: StatelessGame> {
+    move_list: Vec<G::Move>,
+}
+impl<G: StatelessGame> GamePointer<G> {
+    fn traverse_tree_downward<'a>(
+        &self,
+        game: &'a mut GameTree<G>,
+    ) -> Result<&'a mut GameNode<G>, MoveError> {
+        let mut node = &mut game.root;
+        for mv in &self.move_list {
+            let next = node.traverse_downward(mv)?;
+            node = next;
+        }
+        Ok(node)
+    }
+    fn make_move<'a>(
+        &mut self,
+        game: &'a mut GameTree<G>,
+        mv: G::Move,
+    ) -> Result<&'a mut GameNode<G>, MoveError> {
+        let active_board = self.traverse_tree_downward(game)?;
+        let next_board = active_board.make_move(mv)?;
+        self.move_list.push(mv);
+        Ok(next_board)
+    }
+    fn undo_move(&mut self) {
+        let _ = self.move_list.pop();
+    }
+}
+
+pub struct GameTree<Game: StatelessGame> {
+    root: GameNode<Game>,
+}
